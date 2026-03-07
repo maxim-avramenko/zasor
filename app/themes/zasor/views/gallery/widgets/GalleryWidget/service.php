059dcfaf6bf12ae32c4d98a6262d1688f85952ef
sim <maxim.avramenko@gmail.com>
2026-03-07 23:15:46 +0300

    update project gallery

--- app/themes/zasor/views/gallery/widgets/GalleryWidget/service.php
+++ app/themes/zasor/views/gallery/widgets/GalleryWidget/service.php
@@ -1,13 +1,17 @@
 <?php if ($dataProvider->itemCount): ?>
-    <div class="about__gallery slide-up">
-        <?php foreach ($dataProvider->getData() as $data): ?>
+<div class="gallery gallery-serves-page">
+    <div class="container">
+        <div class="about__gallery slide-up">
+            <?php foreach ($dataProvider->getData() as $data): ?>
 
 
-            <a href="<?= $data->image->getImageUrl() ?>" data-fancybox="gallery" class="about__pic">
-                <img src="<?= $data->image->getImageUrl(490, 245, true) ?>"/>
-            </a>
+                <a href="<?= $data->image->getImageUrl() ?>" data-fancybox="gallery" class="about__pic">
+                    <img src="<?= $data->image->getImageUrl(490, 245, true) ?>"/>
+                </a>
 
-        <?php endforeach; ?>
+            <?php endforeach; ?>
+        </div>
     </div>
+</div>
 <?php endif; ?>
 
